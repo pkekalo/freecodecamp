258ca4bc36f4db49dbbe2e88d97240372abe6423
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2018-11-13 16:43:18 +0100

    Make convertToRoman function, Make all tests to pass

--- Projects/javaScript_algorithms_and_data_structures/roman_numeral_converter.js
+++ Projects/javaScript_algorithms_and_data_structures/roman_numeral_converter.js
@@ -21,25 +21,49 @@ function convertToRoman(input) {
       count = count - 1000;
       result += (Object.keys(romanNums)[6]); // 'M'
       countRumanNum(count);
+      } else if (num >= 900) {
+        count = count - 900;
+        result += (Object.keys(romanNums)[4]) + (Object.keys(romanNums)[6]); // 'CM'
+        countRumanNum(count);
       } else if (num >= 500) {
         count = count - 500;
         result += (Object.keys(romanNums)[5]); // 'D'
+        countRumanNum(count);
+      } else if (num >= 400) {
+        count = count - 400;
+        result += (Object.keys(romanNums)[4]) + (Object.keys(romanNums)[5]); // 'CD'
         countRumanNum(count);
       } else if (num >= 100) {
         count = count - 100;
         result += (Object.keys(romanNums)[4]); // 'C'
         countRumanNum(count);
+      } else if (num >= 90) {
+        count = count - 90;
+        result += (Object.keys(romanNums)[2]) + (Object.keys(romanNums)[4]); // 'XC'
+        countRumanNum(count);
       } else if (num >= 50) {
         count = count - 50;
         result += (Object.keys(romanNums)[3]); // 'L'
+        countRumanNum(count);
+      } else if (num >= 40) {
+        count = count - 40;
+        result += (Object.keys(romanNums)[2]) + (Object.keys(romanNums)[3]); // 'XL'
         countRumanNum(count);
       } else if (num >= 10) {
         count = count - 10;
         result += (Object.keys(romanNums)[2]); // 'X'
         countRumanNum(count);
+      } else if (num >= 9) {
+        count = count - 9;
+        result += (Object.keys(romanNums)[0]) + (Object.keys(romanNums)[2]); // 'IX'
+        countRumanNum(count);
       } else if (num >= 5) {
         count = count - 5;
         result += (Object.keys(romanNums)[1]); // 'V'
+        countRumanNum(count);
+      } else if (num >= 4) {
+        count = count - 4;
+        result += (Object.keys(romanNums)[0]) + (Object.keys(romanNums)[1]); // 'IV'
         countRumanNum(count);
       } else if (num >= 1) {
         count = count - 1;
