737b74f16b9ed7b60fff5683b0d1fe77bd893248
Petr Kekalo <kekalo.petr@gmail.com>
2020-03-22 20:55:40 -0400

    add description

--- Projects/javaScript_algorithms_and_data_structures/practice/UseArrowFunctionstoWriteConciseAnonymousFunctions.js
+++ Projects/javaScript_algorithms_and_data_structures/practice/UseArrowFunctionstoWriteConciseAnonymousFunctions.js
@@ -1,3 +1,5 @@
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
 const magic = ()  => {
   return new Date();
 };
--- Projects/javaScript_algorithms_and_data_structures/practice/WriteArrowFunctionswithParameters.js
+++ Projects/javaScript_algorithms_and_data_structures/practice/WriteArrowFunctionswithParameters.js
@@ -1,3 +1,5 @@
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
 const myConcat = (arr1, arr2) => {
    return arr1.concat(arr2);
 };
