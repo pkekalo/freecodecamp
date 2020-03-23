2e8edc563da362c066bee3576871450aeeb13345
Petr Kekalo <kekalo.petr@gmail.com>
2020-03-22 20:58:46 -0400

    (init)

---
+++ Projects/javaScript_algorithms_and_data_structures/practice/UsetheRestParameterwithFunctionParameters.js
@@ -0,0 +1,7 @@
+//Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+
+const sum = (x, y, z) => {
+  const args = [x, y, z];
+  return args.reduce((a, b) => a + b, 0);
+}
+console.log(sum(1, 2, 3)); // 6
