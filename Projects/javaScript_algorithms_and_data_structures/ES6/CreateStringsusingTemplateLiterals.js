77af38f4a364868f2cc7910064f54ab2ae8f79fd
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2020-03-26 23:27:33 -0400

    (init)

---
+++ Projects/javaScript_algorithms_and_data_structures/ES6/CreateStringsusingTemplateLiterals.js
@@ -0,0 +1,20 @@
+// Use template literal syntax with backticks to display each entry of the result object's failure array. Each entry should be wrapped inside an li element with the class attribute text-warning, and listed within the resultDisplayArray.
+// Use an iterator method (any kind of loop) to get the desired output (shown below).
+// (Initial code)
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // Only change code below this line
+  const resultDisplayArray = null;
+  // Only change code above this line
+
+  return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.failure);
