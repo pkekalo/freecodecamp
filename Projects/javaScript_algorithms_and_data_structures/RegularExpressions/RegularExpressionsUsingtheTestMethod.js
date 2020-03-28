fa963701f996655ec87429943d6ab79764c517f9
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2020-03-27 23:32:32 -0400

    (init)

---
+++ Projects/javaScript_algorithms_and_data_structures/RegularExpressions/RegularExpressionsUsingtheTestMethod.js
@@ -0,0 +1,5 @@
+// Apply the regex myRegex on the string myString using the .test() method.
+// (Initial code)
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex; // Change this line
