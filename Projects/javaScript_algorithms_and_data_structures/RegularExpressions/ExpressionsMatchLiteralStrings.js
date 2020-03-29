3ec5c67942219e4cd418385882467435c3a0ee42
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2020-03-28 22:40:30 -0400

    Passed FCC test cases

--- Projects/javaScript_algorithms_and_data_structures/RegularExpressions/ExpressionsMatchLiteralStrings.js
+++ Projects/javaScript_algorithms_and_data_structures/RegularExpressions/ExpressionsMatchLiteralStrings.js
@@ -1,6 +1,8 @@
 //Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match.
-// (Initial code)
+// (Final code)
+// (Passed FCC test cases)
 
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
-let waldoRegex = /search/; // Change this line
+let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
+
