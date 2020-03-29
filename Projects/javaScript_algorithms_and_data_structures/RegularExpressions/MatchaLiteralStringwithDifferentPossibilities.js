de153d7cbe8bb5b26d4fa0fa46da8c164c172526
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2020-03-28 22:45:09 -0400

    Passed FCC test cases

--- Projects/javaScript_algorithms_and_data_structures/RegularExpressions/MatchaLiteralStringwithDifferentPossibilities.js
+++ Projects/javaScript_algorithms_and_data_structures/RegularExpressions/MatchaLiteralStringwithDifferentPossibilities.js
@@ -1,6 +1,8 @@
 // Complete the regex petRegex to match the pets "dog", "cat", "bird", or "fish".
-// (Initial code)
+// (Final code)
+// (Passed FCC test cases)
 
 let petString = "James has a pet cat.";
-let petRegex = /change/; // Change this line
+let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
