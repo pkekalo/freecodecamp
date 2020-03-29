481f786c5b7f630b5e09c1460a5c469a009af85a
Petr Kekalo <35931753+pkekalo@users.noreply.github.com>
2020-03-28 22:42:22 -0400

    (init)

---
+++ Projects/javaScript_algorithms_and_data_structures/RegularExpressions/MatchaLiteralStringwithDifferentPossibilities.js
@@ -0,0 +1,6 @@
+// Complete the regex petRegex to match the pets "dog", "cat", "bird", or "fish".
+// (Initial code)
+
+let petString = "James has a pet cat.";
+let petRegex = /change/; // Change this line
+let result = petRegex.test(petString);
